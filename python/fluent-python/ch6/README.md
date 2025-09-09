4b009e3660c547c276f911329e71c41ffc2de621
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-08 22:00:03 -0700

    Add ch6 and placeholder ch5 notes for Fluent Python

---
+++ python/fluent-python/ch6/README.md
@@ -0,0 +1,1 @@
+# Chapter 6: Object References, Mutability, and Recycling
---
+++ python/fluent-python/ch6/copies.py
@@ -0,0 +1,43 @@
+"""Deep and shallow copy demonstration using Bus class."""
+
+class Bus:
+
+    def __init__(self, passengers=None):
+        if passengers is None:
+            self.passengers = []
+        else:
+            self.passengers = list(passengers)
+
+    def pick(self, name):
+        self.passengers.append(name)
+
+    def drop(self, name):
+        self.passengers.remove(name)
+
+class HauntedBus:
+    """A bus model haunted by ghost passengers"""
+
+    def __init__(self, passengers=[]):
+        self.passengers = passengers
+
+    def pick(self, name):
+        self.passengers.append(name)
+
+    def drop(self, name):
+        self.passengers.remove(name)
+
+class TwilightBus:
+    """A bus model that makes passengers vanish"""
+
+    def __init__(self, passengers=None):
+        if passengers is None:
+            self.passengers = []
+        else:
+            self.passengers = passengers
+
+    def pick(self, name):
+        self.passengers.append(name)
+
+    def drop(self, name):
+        self.passengers.remove(name)
+
