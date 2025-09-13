f93e3b3ba9b2814f75c24891506ff23a2b1facd7
ryan-rashidian <ryanrashidian@gmail.com>
2025-09-13 02:05:02 -0700

    Add data_class.py to Fluent Python /ch5/ as demonstration of data class builders

---
+++ python/fluent-python/ch5/data_class.py
@@ -0,0 +1,26 @@
+"""Data class builders demonstration."""
+
+from dataclasses import dataclass
+from typing import NamedTuple
+
+class Coordinate(NamedTuple):
+    lat: float
+    lon: float
+
+    def __str__(self):
+        ns = 'N' if self.lat >= 0 else 'S'
+        we = 'E' if self.lon >= 0 else 'W'
+        return f'{abs(self.lat):.1f}°{ns}, {abs(self.lon):.1f}°{we}'
+
+# The @dataclass decorator does not depend on inheritance or a metaclass
+# Therefore, it will not interfere with using those mechanisms.
+@dataclass(frozen=True)
+class Coordinate2:
+    lat: float
+    lon: float
+
+    def __str__(self):
+        ns = 'N' if self.lat >= 0 else 'S'
+        we = 'E' if self.lon >= 0 else 'W'
+        return f'{abs(self.lat):.1f}°{ns}, {abs(self.lon):.1f}°{we}'
+
